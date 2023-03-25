a012191025eff16dc669c582836a1c7e100d2741
joethesaint <joebamisaye068@gmail.com>
2023-03-24 22:59:43 -0400

    add task 1, 2, 3, 4 and 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor.name !== 'Building' && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
